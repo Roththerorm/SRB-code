0bc833ad1d402251abaa7e022d74750901c5dc2e
Nicolas Pessoa <nicolas.pessoa00@gmail.com>
2023-09-15 10:54:37 -0300

    UPDATE: Format and correct typo erros

--- article_graphs.py
+++ article_graphs.py
@@ -1,69 +1,71 @@
+'''
+This script generates a figure describing regions of operation of the Yb-Laser.
+It reads the diode data and plots a curve of Power x current.
+created by: Nícolas Pessoa
+'''
+
+
 # Import libraries 
 import matplotlib.pyplot as plt
 import matplotlib
 import pandas as pd
 
-
-# Path
-path = r'C:\Users\nicol\OneDrive\Documentos\1 - Faculdade\Metrologia\Diode_potency_data.csv'
-
-# Import data
-data = pd.read_csv(path, sep=';', decimal=',')
-
-# Plot data
-
+#Define some global parameters of the graphs (font, size, etc.)
 matplotlib.rcParams['font.family'] = 'sans-serif'
 matplotlib.rcParams.update({'font.size' : 14})
 matplotlib.rcParams['axes.linewidth'] = 1.5
 matplotlib.rcParams['hatch.linewidth'] = 1.5
 
+# Define the path where to pull the data
+path = r'C:\Users\nicol\OneDrive\Documentos\1 - Faculdade\Metrologia\Diode_potency_data.csv'
+
+# Import data from path
+data = pd.read_csv(path, sep=';', decimal=',')
+
+################ Plot data ################
+
 fig, ax = plt.subplots(num=1, figsize=[7.6, 4.3])
+ax.plot(data['corrente (mA)'], data['potencia (mW)'], lw=2, color='black', label='Diode output')
 
-ax.plot(data['corrente (mA)'], data['potencia (mW)'], lw=2, color='black',
-label='Diode output')
-
-
-x_positions = [min(data['corrente (mA)']),
-            45, 268, 290, 410, 630, max(data['corrente (mA)'])]
-
-
+# Set positions in the x axis where the transition of regime occurs
+x_positions = [min(data['corrente (mA)']), 45, 268, 290, 410, 630, max(data['corrente (mA)'])]
 
 # List of positions to add the text
 text_pos = [(156, 300,'CW\nREGIME'),
-(351, 285, 'QUASI\nMODE\nLOCKED\nREGIME'),
-(520, 150, 'STANDARD\nMODE-LOCKED\nREGIME'), 
-(755, 150, 'COMPLEX\nDYNAMICS')]
+            (351, 285, 'QUASI\nMODE\nLOCKED\nREGIME'),
+            (520, 150, 'STANDARD\nMODE-LOCKED\nREGIME'), 
+            (755, 150, 'COMPLEX\nDYNAMICS')]
 
-ax.axvspan(x_positions[0], x_positions[1], color='#CED0C8', alpha=0.4)
-ax.axvspan(x_positions[1], x_positions[2], color='#347B98', alpha=0.4)
-ax.axvspan(x_positions[2], x_positions[3], color='#663399', alpha=0.4, hatch="\\\\")
-ax.axvspan(x_positions[3], x_positions[4], color='#DEC121', alpha=0.4)
-ax.axvspan(x_positions[4], x_positions[5], color='#5EBA1C', alpha=0.4)
-ax.axvspan(x_positions[5], x_positions[6], color='#E12514', alpha=0.4)
+# Define a list of colors for the axvspan rectangles
+colors = ['#CED0C8', '#347B98', '#663399', '#DEC121', '#5EBA1C', '#E12514']
 
+# Define a list of hatches for the axvspan rectangles (empty string means no hatch)
+hatches = ['', '', '\\\\', '', '', '']
 
-ax.text(18, 65, 'NO SIGNIFICANT SPECTRUM DETECTED', fontsize=10,rotation='vertical')
-
-
+# Loop through the x_positions list to create axvspan rectangles
+for i in range(len(x_positions) - 1):
+    ax.axvspan(x_positions[i], x_positions[i + 1], color=colors[i], alpha=0.4, hatch=hatches[i])
 
 # Loop through x-coordinates and add vertical lines
-
-x_positions_new = [45, 268, 290, 410, 630]
-for x in x_positions_new:
+for x in x_positions[1:-1]:
     ax.axvline(x=x, color='black')
 
+# Loop through text positions to add text
 for x, y, txt in text_pos:
-    ax.text(x, y, txt, fontsize=12, color='black',
-    ha='center')
+    ax.text(x, y, txt, fontsize=12, color='black', ha='center')
 
+# Add extra text for the first axvspan 
+ax.text(18, 65, 'NO SIGNIFICANT SPECTRUM DETECTED', fontsize=10,rotation='vertical')
 
-ax.set_xlim(left=1, 
-            right=max(data['corrente (mA)']))
-ax.set_ylim(bottom=0,
-            top=max(data['potencia (mW)']))
+################ Plot configuration ################
+
+ax.set_xlim(left=1, right=data['corrente (mA)'].max())
+ax.set_ylim(bottom=0,top=data['potencia (mW)'].max())
+
 ax.set_xlabel('Current (mA)', labelpad=15)
 ax.set_ylabel('Power (mW)', labelpad=15)
 ax.tick_params(axis='both', direction='in')
+
 ax.legend(loc='best')
 plt.tight_layout()
 plt.savefig(rf'C:\Users\nicol\Desktop\Figuras (Marcio)\regions of operations.pdf')
